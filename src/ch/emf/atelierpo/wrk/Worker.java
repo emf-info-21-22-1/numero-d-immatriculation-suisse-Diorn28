aa00422823ec1d3ece3dd9cac8d8c4c0aac4ed53
EspinO <EspinO@WSTEMFB32-03.stu.net.fr.ch>
2023-03-28 09:32:22 +0200

    Javadoc finie

--- src/ch/emf/atelierpo/wrk/Worker.java
+++ src/ch/emf/atelierpo/wrk/Worker.java
@@ -8,7 +8,7 @@ public class Worker {
 
    /**
     * Cette méthode est utilisé pour vérifier si une plaque d'immatriculation est valide.
-    * Pour que la méthode fonctionne il va falloir vérifier que le nom du canton soit valide à l'aide d'un IF et que le String pour le numéro de plaque est belle et bien comp
+    * Pour que la méthode fonctionne il va falloir vérifier que le nom du canton soit valide à l'aide d'un IF et que le String pour le numéro de plaque est belle et bien composé que de charactère et non pas de chiffre à l'aide d'un try catch
     * @param canton Il s'agit des innitiaux des cantons. Pour que ceux-ci soient valident il faut que les charactères utilisé soit comprise entre A et Z.
     * @param numeroDePlaque Il s'agit du numéro de la plaque d'immatriculation à testé, stocké dans un String. Celui-ci ne doit contenir que des numéros.
     * @return 
